5eaa20f57cedaa0afe9262275536e560ad18a32b
AnnaPuchkova <puchkova.anne@gmail.com>
2022-10-21 14:03:54 +0300

    no message

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,13 @@
-const { defineConfig } = require('@vue/cli-service');
+const {
+  defineConfig
+} = require('@vue/cli-service');
 
 module.exports = defineConfig({
   transpileDependencies: [
     'vuetify',
   ],
 });
+
+module.exports = {
+  publicPath: '/avpuchkova.github.io/'
+}
